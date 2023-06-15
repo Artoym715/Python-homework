0b1762eb32b589244345f4ef598c8e65bde0f312
Dmitry Bystrov <98824571+Artoym715@users.noreply.github.com>
2023-06-16 00:31:04 +0600

    Задача 3

--- HomeWork-2/main.py
+++ HomeWork-2/main.py
@@ -42,3 +42,19 @@ else:
                 j = 1001
         else:
             i = 1001
+
+# Задача: Требуется вывести все целые степени двойки (т.е. числа вида 2k), не превосходящие числа N.
+
+n = abs(int(input('Введите число n ')))
+stop = 0
+P = 2
+for i in range(n):
+    if stop != 1:
+        P = P ** i
+        if P <= n:
+            print(P, end=' ')
+            P = 2
+        else:
+            stop = 1
+    else:
+        i = n
