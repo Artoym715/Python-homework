f20c5afa78620ac84197e292aae8a584d2e4e139
Dmitry Bystrov <98824571+Artoym715@users.noreply.github.com>
2023-07-04 23:01:48 +0600

    2 задача

--- HomeWork-7/main.py
+++ HomeWork-7/main.py
@@ -23,3 +23,21 @@ def rytm(str):
 
 rytm(string)
 
+# Задача: Напишите функцию print_operation_table(operation, num_rows=6, num_columns=6), которая принимает в качестве аргумента функцию, вычисляющую элемент по номеру строки и столбца.
+# Аргументы num_rows и num_columns указывают число строк и столбцов таблицы, которые должны быть распечатаны.
+# Нумерация строк и столбцов идет с единицы (подумайте, почему не с нуля).
+# Примечание: бинарной операцией называется любая операция, у которой ровно два аргумента, как, например, у операции умножения.
+
+line, column = (map(int, input("Введите строку и столбец через пробел: ").split()))
+
+
+def print_operation_table(operation, num_rows, num_columns):
+    list1 = [[operation(i, j) for i in range(1, num_rows + 1)] for j in range(1, num_columns + 1)]
+    for i in range(len(list1)):
+        for j in range(len(list1[i])):
+            print("{:4d}".format(list1[i][j]), end="")
+        print()
+
+
+print_operation_table(lambda x, y: x * y, line, column)
+
