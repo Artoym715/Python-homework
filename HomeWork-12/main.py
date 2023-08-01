ddebbd66f6575f11efbbb8972daa533a6c26be49
Dmitry Bystrov <98824571+Artoym715@users.noreply.github.com>
2023-08-01 19:09:25 +0600

    2 задача

--- HomeWork-12/main.py
+++ HomeWork-12/main.py
@@ -13,5 +13,20 @@ print(f"Конечная матрица - {matrixTranspose(theMatrix)}")
 # где ключ — хэш значения переданного аргумента, а значение — имя аргумента.
 # Если ключ не хешируем, используйте его строковое представление.
 
-def dz_2(**kwargs: dict) -> dict:
-    return {x: v for v, x in kwargs.items()}
+def convert_dict(dictionary) -> dict:
+    return {x: v for v, x in dictionary.items()}
+
+dictionary = {'персона': 'человек',
+              'марафон': 'гонка бегунов длиной около 26 миль',
+              'противостоять': 'оставаться сильным, несмотря на давление',
+              'бежать': 'двигаться со скоростью'}
+
+print("Начальный словарь: ")
+for key, value in dictionary.items():
+        print(f"{key}: {value}")
+
+new_dict = convert_dict(dictionary)
+
+print("Конечный словарь: ")
+for key, value in new_dict.items():
+        print(f"{key}: {hash(value)}")
