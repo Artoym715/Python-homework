9372cf80f814a5dd5730ff7384e676f14fca4df6
Dmitry Bystrov <98824571+Artoym715@users.noreply.github.com>
2023-09-11 00:20:37 +0600

    Добавление документации

--- HomeWork-19/matrix_class/Matrix.py
+++ HomeWork-19/matrix_class/Matrix.py
@@ -2,20 +2,27 @@ __all__ = ['Matrix']
 
 
 class Matrix:
-
+    """Класс которые на входе получает матрицу и содержит методы для:
+        - сравнения,
+        - сложения,
+        - умножения матриц."""
     def __init__(self, matr):
+        """Добавляем the matr атрибут."""
         self._matr = matr
 
     def get_matrix(self):
+        """Геттер получаем матрицу"""
         return self._matr
 
     def __add__(self, other):
+        """Метод для сложения матриц"""
         if len(self._matr) != len(other._matr) or len(self._matr[0]) != len(other._matr[0]):
             return f'Error: матрицы разных размеров'
         else:
             return Matrix([[self._matr[i][j] + other._matr[i][j] for j in range(len(self._matr[0]))] for i in range(len(self._matr))])
 
     def __mul__(self, other):
+        """Метод для умножения матриц"""
         if len(self._matr[0]) != len(other._matr):
             return f'Error: невозможно перемножить матрицы'
         else:
@@ -23,6 +30,7 @@ class Matrix:
             return Matrix(new_matr)
 
     def __eq__(self, other):
+        """Метод для сравнения матриц"""
         if len(self._matr) != len(other._matr) or len(self._matr[0]) != len(other._matr[0]):
             return f'Error: матрицы разных размеров'
         else:
